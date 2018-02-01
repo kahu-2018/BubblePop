8e2ac185ae1fcda351b3e3bb099379991d0c68fb
kahu2018 <fake@fake.com>
2018-02-02 11:50:51 +1300

    Not using

---
+++ client/components/Score.jsx
@@ -0,0 +1,21 @@
+import React from 'react'
+import bubble from bubble
+
+class Score extends React.Component {
+
+  constructor (props) {
+    super(props)
+    this.state = {
+      count: 0
+    }
+   this.addScore = this.addScore.bind(this)
+
+
+
+  }
+  addScore ()
+  when this.props.popped == true
+  this.setStatetate.count ++
+}
+
+export default Score
