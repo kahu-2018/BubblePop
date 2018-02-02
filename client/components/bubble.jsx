3dcf264c1f38d018f9eacf10302d8f2f62c5df03
Melissa Mepham <m.a.mepham@gmail.com>
2018-02-02 14:20:52 +1300

    MVP

--- client/components/bubble.jsx
+++ client/components/bubble.jsx
@@ -1,53 +1,44 @@
 import React from 'react'
-var requestAnimationFrame = window.requestAnimationFrame
+
 
   class Bubble extends React.Component {
     constructor(props) {
       super(props)
-      console.log("constructed");
       this.state={
         r: props.r,
         cx: props.cx,
-        cy: props.cy
+        cy: props.cy,
+        opacity: 0.5,
+        bubbleStroke: 1,
       }
+      this.click = this.click.bind(this)
     }
     componentWillReceiveProps(nextProps) {
       const {r, cx, cy} = nextProps
       this.setState({r, cx, cy})
     }
-
-
+    click() {
+      this.setState({opacity: 0, bubbleStroke: 0})
+      this.props.pop()
+    }
     render() {
-      var pop = () => { alert("pop!") }
-      // console.log(this.state, this.props)
+
+      // var pop = (props) => {
+      //
+      //   console.log('yup')
+      //    this.state.opacity = 0
+      //    this.state.bubbleStroke = 0
+      //    score ++
+      //    //this.setState({score: this.state.score ++})
+      //    console.log(score)
+      //  }
       return (
-        <circle onClick={pop} r={this.props.r} cx={this.props.cx} cy={this.props.cy} fill="#FF53FF" fillOpacity={.5} stroke="#AA1561"/>
+        <circle onClick={this.click} r={this.props.r} cx={this.props.cx} cy={this.props.cy} fill="#FF53FF" fillOpacity={this.state.opacity} strokeOpacity={this.state.bubbleStroke} stroke="#AA1561"/>
       )
     }
   }
 
-  // function Bubble (props) {
-  //   return (
-  //     <circle r={props.r} cx={props.cx} cy={props.cy} fill="#FF53FF" fillOpacity={.5} stroke="#AA1561"/>
-  //
-  //   )
-  // }
-    // this.moveBubble=this.moveBubble.bind(this)
-    // this.animateBubble = this.animateBubble.bind(this)
-    // function animateBubble() {
-    //   let {cx, cy, r} = this.props.circle
-    //   let bubble = [
-    //     { r: r++ }
-    //   ]
-    //   requestAnimationFrame(animateBubble)
 
-// function moveBubble() {
-//   return (
-//   <circle r={this.state.r} cx={this.state.cx} cy={this.state.cy} fill="#FF53FF" fillOpacity={.5} stroke="#AA1561"/>
-// }
-
-    // }
-
-
-
-export default Bubble
+module.exports= {
+  Bubble: Bubble,
+}
